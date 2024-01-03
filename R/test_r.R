008cb15bd4055fbce2d010185a5fcef9e693bb4f
doughunt01 <douglas.huntington@pfizer.com>
2024-01-03 17:27:37 +0000

    Test commit

---
+++ R/test_r.R
@@ -0,0 +1,16 @@
+# Setup github username and email 
+library(usethis)
+library(gitcreds) 
+library(credentials) 
+
+## Use terminal comands (similar to citrix server)
+# Remove repo using rm - repo/
+
+## use which git / git --version in terminal (shell) to detect git
+
+# Setting up github account access 
+usethis::use_git_config(user.name = "doughunt01", user.email = "douglas.huntington@pfizer.com")
+
+
+# Setting PAT as password
+gitcreds::gitcreds_set()

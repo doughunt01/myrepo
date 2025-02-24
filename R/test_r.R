a8c3fcccb60e67ae1b7259987ee45c0d8bcf7718
doughunt01 <douglas.huntington@pfizer.com>
2025-02-24 14:42:13 +0000

    test

--- R/test_r.R
+++ R/test_r.R
@@ -2,7 +2,8 @@
 library(usethis)
 library(gitcreds) 
 library(credentials) 
-
+library(tidyverse)
+ 
 ## Use terminal comands (similar to citrix server)
 # Remove repo using rm - repo/
 
@@ -11,8 +12,16 @@ library(credentials)
 # Setting up github account access 
 usethis::use_git_config(user.name = "doughunt01", user.email = "douglas.huntington@pfizer.com")
 
-
+ 
 # Setting PAT as password
 gitcreds::gitcreds_set() 
 
 ## Seeing if this changes 
+
+
+## Explaining how git works in RStudio Workbench
+# PAT token - key to entire github, once set you can commit, push, pull from github
+# Need to clone repo locally first, so you can then push/pull from repo on github
+# Need to commit changes to local repo first, then push everything when you are done to github
+
+# With README.md, just push to github once done editing it. Will not appear in path  
